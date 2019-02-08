5e6b5ac353c6cba7fe9a7d97c6f780896d353fdd
Zachary <zbberman@yahoo.com>
2019-02-08 17:33:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -45,6 +45,22 @@ end
 
 def get_english_meaning(file_path, emoticon)
   emoticon_hash = load_library(file_path)
+  
+  return_meaning = nil 
+  
+  #iterate through the "get_meaning" hash and find the japanese emoticon 
+  emoticon_hash["get_emoticon"].each do | english_emoticon, japanese_emoticon |
+    if emoticon == english_emoticon
+      return_emoticon = japanese_emoticon
+    end 
+  end 
+  
+  #case if no emoticon is found
+  if return_emoticon == nil 
+    "Sorry, that emoticon was not found"
+  else 
+    return_emoticon
+  end 
 end
 
 # it "accepts two arguments, the YAML file path and the emoticon" do
