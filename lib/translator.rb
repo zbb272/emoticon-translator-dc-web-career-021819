b22693d196f6ed83acc6ea4420667fe28943dbfe
Zachary <zbberman@yahoo.com>
2019-02-08 17:17:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,6 +27,38 @@ def get_japanese_emoticon
   # code goes here
 end
 
+# describe "#get_japanese_emoticon" do
+
+#   it "accepts two arguments, the YAML file path and the emoticon" do
+#     expect { get_japanese_emoticon("./lib/emoticons.yml", ":)") }.to_not raise_error
+#   end
+
+#   it "calls on #load_library and gives it the argument of the file path" do
+#     emoticon_hash = load_library("./lib/emoticons.yml")
+
+#     file_path = "./lib/emoticons.yml"
+#     expect(self).to receive(:load_library).with(file_path).and_return(emoticon_hash)
+#     answer = get_japanese_emoticon("./lib/emoticons.yml", "=D")
+#   end
+
+#   it "returns the Japanese equivalent of an English grinning" do
+#     expect(get_japanese_emoticon("./lib/emoticons.yml", "=D")).to eq("(￣ー￣)")
+#   end
+
+#   it "returns the Japanese equivalent of an English happy" do
+#     expect(get_japanese_emoticon("./lib/emoticons.yml", ":)")).to eq("(＾ｖ＾)")
+#   end
+
+#   it "returns the Japanese equivalent of an English sad" do
+#     expect(get_japanese_emoticon("./lib/emoticons.yml", ":'(")).to eq("(Ｔ▽Ｔ)")
+#   end
+
+#   it "returns an apology message if the argument is not a known emoticon" do
+#     sorry_message = "Sorry, that emoticon was not found"
+#     expect(get_japanese_emoticon("./lib/emoticons.yml", "$#$%{}*")).to eq(sorry_message)
+#   end
+
+
 def get_english_meaning
   # code goes here
 end
